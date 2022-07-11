bec3962c35e077c5e347f12b8973859a0157de2c
yanting <yy3189@columbia.edu>
2022-07-10 21:16:12 -0400

    `plugins_after.zsh`: remove unused plugins

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -1,7 +1,4 @@
 # External plugins (initialized after)
 
-# Auto suggestion
-# source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
 # Auto Completion
 source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
