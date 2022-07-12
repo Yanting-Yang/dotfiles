3793d5a510a8c63a4a0404504d037b85f569dada
Yanting Yang <yy3189@columbia.edu>
2022-07-12 10:23:08 -0400

    `tmux.zsh`: check if already in tmux environment

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -4,13 +4,20 @@ command -v tmux &> /dev/null
 
 if [[ $? == 0 ]]
 then
-    local SESSIONNAME="main"
-    tmux has-session -t $SESSIONNAME &> /dev/null
-    if [[ $? != 0 ]]
+    if [[ -z "$TMUX" ]]
     then
-        tmux new-session -s $SESSIONNAME -d
+        local SESSIONNAME="main"
+        tmux has-session -t $SESSIONNAME &> /dev/null
+        if [[ $? != 0 ]]
+        then
+            tmux new-session -s $SESSIONNAME -d
+        fi
+        tmux attach -t $SESSIONNAME
+    else
+        echo "[TMUX] you are in tmux environment"
+        echo "[TMUX] session name: $(tmux display-message -p '#S')"
+        echo "[TMUX] number of clients session is attached to: $(tmux display-message -p '#{session_attached}')"
     fi
-    tmux attach -t $SESSIONNAME
 else
-    echo "command tmux not found"
+    echo "[ERROR] command tmux not found"
 fi
